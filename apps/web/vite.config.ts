897f2e0a49e001e133c32a1ae2816fb87f7381cd
hyooeewee <hughie-ngai@outlook.com>
2026-04-15 10:52:01 +0800

    chore: migrate to vite plus

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,8 +1,21 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig } from "vite-plus";
 
 export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  fmt: {
+    ignorePatterns: ["app/components"],
+  },
+  lint: {
+    ignorePatterns: ["app/components"],
+    options: {
+      typeAware: true,
+      typeCheck: true,
+    },
+  },
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
