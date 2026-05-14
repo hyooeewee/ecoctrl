8f318d781bbf5574f5a8e79689e663898565ea96
hyooeewee <hughie-ngai@outlook.com>
2026-05-14 09:39:28 +0800

    fix: update drizzle.config.ts to include type definition and override option for dotenv config

--- packages/server/drizzle.config.ts
+++ packages/server/drizzle.config.ts
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
-import { defineConfig } from "drizzle-kit";
+import { defineConfig, type Config } from "drizzle-kit";
 
-config({ path: ".env.local" });
+config({ path: ".env.local", override: false });
 
 export default defineConfig({
   schema: "./src/schemas/*",
@@ -10,4 +10,4 @@ export default defineConfig({
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
-});
+}) satisfies Config;
