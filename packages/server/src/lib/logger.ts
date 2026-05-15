2de6a727a6bc83f885c4c668a6c985d1fddcc31b
hyooeewee <hughie-ngai@outlook.com>
2026-05-15 22:51:13 +0800

    feat(logger): load environment variables from .env.local file

--- packages/server/src/lib/logger.ts
+++ packages/server/src/lib/logger.ts
@@ -1,3 +1,6 @@
+import { config } from "dotenv";
+config({ path: ".env.local" });
+
 import pino from "pino";
 import type { Logger, LoggerOptions } from "pino";
 import pinoPretty from "pino-pretty";
